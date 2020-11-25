cf40e035821e67a38a3b1a4e755d982323fbdeb5
Dawid Krysiński <dawid.krysinski@iteo.com>
2020-11-25 10:58:24 +0100

    Add frequency handling

--- packages/sensors/android/src/main/java/io/flutter/plugins/sensors/StreamHandlerImpl.java
+++ packages/sensors/android/src/main/java/io/flutter/plugins/sensors/StreamHandlerImpl.java
@@ -8,43 +8,50 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+
 import io.flutter.plugin.common.EventChannel;
 
 class StreamHandlerImpl implements EventChannel.StreamHandler {
 
-  private SensorEventListener sensorEventListener;
-  private final SensorManager sensorManager;
-  private final Sensor sensor;
+    private SensorEventListener sensorEventListener;
+    private final SensorManager sensorManager;
+    private final Sensor sensor;
 
-  StreamHandlerImpl(SensorManager sensorManager, int sensorType) {
-    this.sensorManager = sensorManager;
-    sensor = sensorManager.getDefaultSensor(sensorType);
-  }
+    StreamHandlerImpl(SensorManager sensorManager, int sensorType) {
+        this.sensorManager = sensorManager;
+        sensor = sensorManager.getDefaultSensor(sensorType);
+    }
 
-  @Override
-  public void onListen(Object arguments, EventChannel.EventSink events) {
-    sensorEventListener = createSensorEventListener(events);
-    sensorManager.registerListener(sensorEventListener, sensor, sensorManager.SENSOR_DELAY_FASTEST);
-  }
-
-  @Override
-  public void onCancel(Object arguments) {
-    sensorManager.unregisterListener(sensorEventListener);
-  }
-
-  SensorEventListener createSensorEventListener(final EventChannel.EventSink events) {
-    return new SensorEventListener() {
-      @Override
-      public void onAccuracyChanged(Sensor sensor, int accuracy) {}
-
-      @Override
-      public void onSensorChanged(SensorEvent event) {
-        double[] sensorValues = new double[event.values.length];
-        for (int i = 0; i < event.values.length; i++) {
-          sensorValues[i] = event.values[i];
+    @Override
+    public void onListen(Object arguments, EventChannel.EventSink events) {
+        int samplingPeriod = SensorManager.SENSOR_DELAY_FASTEST;
+        if (arguments != null) {
+            samplingPeriod = 1000000 / ((int) arguments);
         }
-        events.success(sensorValues);
-      }
-    };
-  }
+
+        sensorEventListener = createSensorEventListener(events);
+        sensorManager.registerListener(sensorEventListener, sensor, samplingPeriod);
+    }
+
+    @Override
+    public void onCancel(Object arguments) {
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+
+    SensorEventListener createSensorEventListener(final EventChannel.EventSink events) {
+        return new SensorEventListener() {
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                double[] sensorValues = new double[event.values.length];
+                for (int i = 0; i < event.values.length; i++) {
+                    sensorValues[i] = event.values[i];
+                }
+                events.success(sensorValues);
+            }
+        };
+    }
 }
